a9c186bf8bc71a572870155fa0f90cfd9d6313ff
Uncle Common <krishnerkar@gmail.com>
2026-01-13 19:22:10 +0000

    Update header logo with new image

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -5,7 +5,7 @@
   <!-- Main Navigation -->
   <nav class="bg-white py-6 px-6 md:px-10 flex items-center justify-start sticky top-0 z-50">
     <div class="w-[120px] md:w-[140px]">
-      <img src="/uploaded/1767995144430-26saxw3.png" alt="drvrs" class="w-full h-auto" />
+      <img src="/uploaded/1768332109018-n611q5z.png" alt="drvrs" class="w-full h-auto" />
     </div>
   </nav>
 </div>
